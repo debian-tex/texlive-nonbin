72869d1d5a8c80bf6b8727090ac65bdd84efe06d
Norbert Preining <preining@debian.org>
2012-04-10 00:12:47 +0000

    tests cleanup

--- tests/scripts/simple-test-script.sh
+++ tests/scripts/simple-test-script.sh
@@ -9,7 +9,7 @@ debname="$1"
 # and then exit
 #
 report_errors() {
-    for i in /tmp/texlive.* /tmp/updmap* /tmp/fmtutil* /tmp/mktexlsr* ; do
+    for i in /tmp/texlive.* /tmp/updmap* /tmp/mktexlsr* /tmp/fmtutil* ; do
 	echo "======= ERROR FILE $i ==========="
 	cat $i
 	echo "======= END OF ERROR FILE $i ==========="
@@ -17,45 +17,31 @@ report_errors() {
     exit 1
 }
 
-if [ -d /additionalpackages ] ; then
-	echo "deb file:/ additionalpackages/" >> /etc/apt/sources.list
-	gunzip -c /additionalpackages/Packages.gz > /var/lib/apt/lists/_additionalpackages_Packages
-fi
-
-
-# first install tex-common for now until texinfo is fixed
-apt-get install $aptargs tex-common
-
+# install original package
+apt-get install $aptargs $debname || report_errors
 
 #
 # install the sources.list line
 echo "deb file:/ pool/" >> /etc/apt/sources.list
-if [ -d /check ] ; then
-	echo "deb file:/ check/" >> /etc/apt/sources.list
-fi
-
-#
-# update aptitudes lists
-# without network we have a problem
-#aptitude update
 gunzip -c /pool/Packages.gz > /var/lib/apt/lists/_pool_Packages
-if [ -d /check ] ; then
-	gunzip -c /check/Packages.gz > /var/lib/apt/lists/_check_Packages
-fi
 
 #
 # now run the tests
 #
 # install cycle
 #
-# install package
-apt-get install $aptargs $debname || report_errors
+#aptitude purge $aptargs '~ntexlive'
+# upgrade test
+apt-get dist-upgrade $aptargs || report_errors
 # remove
 apt-get remove $aptargs $debname || report_errors
 # install after remove
 apt-get install $aptargs $debname || report_errors
 # purge
 apt-get remove --purge $aptargs $debname || report_errors
+# install new from purged state
+apt-get install $aptargs $debname || report_errors
+# check for left over files?
 
 echo "SUCCESS"
 
